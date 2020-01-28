cf83a4decde31b82a050ecd11d3c31a7e915b928
Daniel Chavez <danchavez_hp@hotmail.com>
2020-01-28 14:21:53 -0600

    Cambios

---
+++ Appli_KT2/Appli_KT2/Utils/NotificacionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appli_KT2.Utils
+{
+    public interface INotificacionManager
+    {
+        event EventHandler NoticationReceived;
+        void Initialize();
+
+        int ScheduleNotification(string title, string message);
+
+        void ReceiveNotification(string title, string message);
+    }
+}
